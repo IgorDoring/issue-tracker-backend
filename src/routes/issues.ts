2f1abab0c541f43710e8e3cb7098861197eb562b
IgorDoring <idoring@tutanota.com>
2026-05-20 18:51:17 -0300

    feat: adding updating and delete

--- src/routes/issues.ts
+++ src/routes/issues.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express'
 import { IssuesModel } from '../data/orm/models/issues'
-import { idParamSchema, issueSchema } from '../validation/issues_validation'
+import { idParamSchema, issueSchema, updateIssueSchema } from '../validation/issues_validation'
 
 export const createIssuesRoutes = (app: Express) => {
     app.get('/issues', async (req, res) => {
@@ -28,6 +28,35 @@ export const createIssuesRoutes = (app: Express) => {
             res.status(201).json(addedIssue)
         } catch (error) {
             res.status(500).json({ message: 'Failed to create issue' })
+        }
+    })
+    app.put('/issues/:id', async (req, res) => {
+        const toUpdateIssue = req.body
+        const issueNo = req.params.id
+
+        const { error, value } = updateIssueSchema.validate(toUpdateIssue)
+        if (error) return res.status(400).json({ message: error.message })
+
+        try {
+            const issue = await IssuesModel.findByPk(issueNo)
+            if (!issue) return res.status(404).json({ message: 'Issue not found' })
+            await IssuesModel.update({ ...value }, { where: { issueNo } })
+            const updatedIssue = await IssuesModel.findByPk(req.params.id)
+            res.status(200).json(updatedIssue)
+        } catch (error) {
+            res.status(500).json({ message: 'Failed to update issue' })
+        }
+    })
+    app.delete('/issues/:id', async (req, res) => {
+        const issueNo = req.params.id
+
+        try {
+            const issue = await IssuesModel.findByPk(issueNo)
+            if (!issue) return res.status(404).json({ message: 'Issue not found' })
+            await IssuesModel.destroy({ where: { issueNo } })
+            res.status(204).send()
+        } catch (error) {
+            res.status(500).json({ message: 'Failed to delete issue' })
         }
     })
 }
