4b98fcc3f2ccdce37cb9538e923983d8a003f845
IgorDoring <idoring@tutanota.com>
2026-05-20 18:25:16 -0300

    feat: adding creation option

--- src/routes/issues.ts
+++ src/routes/issues.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express'
 import { IssuesModel } from '../data/orm/models/issues'
-import { idParamSchema } from '../validation/issues_validation'
+import { idParamSchema, issueSchema } from '../validation/issues_validation'
 
 export const createIssuesRoutes = (app: Express) => {
     app.get('/issues', async (req, res) => {
@@ -10,11 +10,24 @@ export const createIssuesRoutes = (app: Express) => {
     app.get('/issues/:id', async (req, res) => {
         const id = req.params.id
         const { error } = idParamSchema.validate(id)
-        if (error) {
-            res.status(500).json({ message: error.message })
-        } else {
+        if (error) return res.status(400).json({ message: error.message })
+        try {
             const issue = await IssuesModel.findByPk(id)
-            res.status(400).json(issue)
+            if (!issue) return res.status(404).json({ message: 'Issue not found' })
+            res.status(200).json(issue)
+        } catch (error) {
+            res.status(500).json({ message: 'Failed to fetch issue ' + id })
+        }
+    })
+    app.post('/issues', async (req, res) => {
+        const newIssue = req.body
+        const { error, value } = issueSchema.validate(newIssue)
+        if (error) return res.status(400).json({ message: error.message })
+        try {
+            const addedIssue = await IssuesModel.create({ ...value })
+            res.status(201).json(addedIssue)
+        } catch (error) {
+            res.status(500).json({ message: 'Failed to create issue' })
         }
     })
 }
