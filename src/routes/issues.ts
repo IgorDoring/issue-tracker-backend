6755f4a1e7ded72aab4076ca79a1416f385eb03c
IgorDoring <idoring@tutanota.com>
2026-05-20 17:56:55 -0300

    feat: adding validation with joi, and adding get single issue

--- src/routes/issues.ts
+++ src/routes/issues.ts
@@ -1,9 +1,20 @@
 import { Express } from 'express'
 import { IssuesModel } from '../data/orm/models/issues'
+import { idParamSchema } from '../validation/issues_validation'
 
 export const createIssuesRoutes = (app: Express) => {
     app.get('/issues', async (req, res) => {
         const issues = await IssuesModel.findAll()
         res.json(issues)
+    })
+    app.get('/issues/:id', async (req, res) => {
+        const id = req.params.id
+        const { error } = idParamSchema.validate(id)
+        if (error) {
+            res.status(500).json({ message: error.message })
+        } else {
+            const issue = await IssuesModel.findByPk(id)
+            res.status(400).json(issue)
+        }
     })
 }
---
+++ src/validation/issues_validation.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi'
+
+export const issueSchema = Joi.object({
+    title: Joi.string().min(3).required(),
+    description: Joi.string(),
+    priority: Joi.string().valid('high', 'low').required(),
+    type: Joi.string().valid('Feature', 'Bug', 'Documentation').required(),
+    completed: Joi.date().less('now')
+})
+
+export const updateIssueSchema = Joi.object({
+    title: Joi.string().min(3).optional(),
+    description: Joi.string().optional(),
+    priority: Joi.string().valid('high', 'low').optional(),
+    type: Joi.string().valid('Feature', 'Bug', 'Documentation').optional(),
+    completed: Joi.date().less('now')
+})
+
+export const paginationSchema = Joi.object({
+    page: Joi.number().positive().min(1).default(1),
+    pageSize: Joi.number().min(1).max(50).default(10)
+})
+
+export const idParamSchema = Joi.number().positive()
